98566d89ff9ec1b14ad0d616fbb7709c6d9717f4
Chris Samios <samios@gmail.com>
2019-09-25 10:23:21 +1000

    Create remove_memberonly_items_from_guest.rb

---
+++ remove_memberonly_items_from_guest.rb
@@ -0,0 +1,10 @@
+customer = Input.cart.customer
+if customer
+  # If logged in, this script doesn't need to be run.
+  exit
+end
+
+
+Input.cart.line_items.delete_if { |item| item.variant.product.product_type == "Members Only" }
+
+Output.cart = Input.cart
